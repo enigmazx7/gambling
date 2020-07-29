3a113c8b25030e9c2ba3b6a2d1b108ee6f6be94a
enigmazx7 <matheuseg@gmail.com>
2020-07-28 23:22:42 -0300

    Atualização Dados Json

--- src/App.js
+++ src/App.js
@@ -23,28 +23,15 @@ function App() {
       />
 
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[1]}
       />
 
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[2]}
       />
 
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[3]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[4]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[5]}
       />
 
       <Footer as="a" />
