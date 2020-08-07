45b91ba393a8a70f94173021566f09edd41b85fe
enigmazx7 <matheuseg@gmail.com>
2020-08-07 00:06:21 -0300

    Atu DB Json Hiroku

--- src/pages/cadastro/categorias/index.js
+++ src/pages/cadastro/categorias/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8000/categorias';
+    const URL_TOP = 'http://localhost:8080/categorias';
 
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
