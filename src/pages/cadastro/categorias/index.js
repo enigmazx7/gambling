e1316f4a40c105b29318fb010759e845f836bb96
enigmazx7 <matheuseg@gmail.com>
2020-08-07 00:14:08 -0300

    Atu URL DB Json

--- src/pages/cadastro/categorias/index.js
+++ src/pages/cadastro/categorias/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = 'https://gamblingflix.herokuapp.com/categorias';
 
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
